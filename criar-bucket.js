38f7ace00002dac5808d99512ece07f4d3d1db06
Erivaldo <erivaldo.junior@agilize.com.br>
2020-04-28 12:18:25 -0300

    Ajuste do Bucket

--- criar-bucket.js
+++ criar-bucket.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 app.get('/',function(req,res){
 	
     var params = {
-        Bucket: 'BUCKET_STUDY'
+        Bucket: 'bucket_study'
     };
     s3.createBucket(params, function(err, data) {
         if (err) {
